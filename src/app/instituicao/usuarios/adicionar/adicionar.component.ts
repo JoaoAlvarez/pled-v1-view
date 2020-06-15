9cee44e08220ecc3dee5998f9d768f170a71c771
steniomelo <stenio@steniomelo.com.br>
2020-06-15 15:54:35 -0300

    Corrigindo tela de inserir professor

--- src/app/instituicao/usuarios/adicionar/adicionar.component.ts
+++ src/app/instituicao/usuarios/adicionar/adicionar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormArray, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 import { InstituicaoService } from "../../instituicao.service";
 import { finalize } from 'rxjs/operators';
 import { LocalDataSource } from "ng2-smart-table";
@@ -19,85 +19,80 @@ export class AdicionarComponent implements OnInit {
 
   form!: FormGroup;
   isLoading: Boolean = false;
-
-
-  constructor(private formBuilder: FormBuilder, private InstituicaoService: InstituicaoService, protected router: Router,private fb: FormBuilder, private fb2: FormBuilder
-
-  ) { }
-
   productForm: FormGroup;
-  productForm2: FormGroup;
+  turmas = [];
+  disciplinas = [];
+
+  constructor(private formBuilder: FormBuilder, private InstituicaoService: InstituicaoService, protected router: Router) { }
 
   ngOnInit(): void {
-    this.productForm = this.fb.group({
-      title: [],
-      selling_points: this.fb.array([this.fb.group({point:''})])
-    })
-    this.productForm2 = this.fb.group({
-      title: [],
-      selling_points2: this.fb.array([this.fb.group({point:''})])
-    })
-    
     this.getDisciplinas();
+    this.getTurmas();
     this.createForm();
-
-
   }
-list=[];
+
+  getTurmas() {
+    this.InstituicaoService
+      .getTurmas()
+      .pipe(finalize(() => { }))
+      .subscribe((response) => {
+        //this.turmas = response;
+
+        response.forEach(element => {
+          element.series.forEach(element => {
+            element.turmas.forEach(element => {
+              this.turmas.push(element);
+
+            });
+
+          });
+        });
+
+        console.log(this.turmas);
+      });
+  }
 
   getDisciplinas() {
     this.InstituicaoService
       .getDisciplinas()
       .pipe(finalize(() => { }))
       .subscribe((response) => {
-        console.log(response);
-        this.list=response;
+        this.disciplinas = response;
       });
   }
 
-  get sellingPoints() {
-    return this.productForm.get('selling_points') as FormArray;
-  }
-  get sellingPoints2() {
-    return this.productForm2.get('selling_points2') as FormArray;
+  get turmasDisciplinas() {
+    return this.form.get('turmasDisciplinas') as FormArray;
   }
 
-  addSellingPoint() {
-    this.sellingPoints.push(this.fb.group({point:''}));
+  addTurmasDisciplinas() {
+    this.turmasDisciplinas.push(this.formBuilder.group({
+      turma: ['', Validators.required],
+      disciplina: ['', Validators.required]
+    }));
   }
-  addSellingPoint2() {
-    this.sellingPoints2.push(this.fb.group({point:''}));
-  }
-  deleteSellingPoint(index) {
-    this.sellingPoints.removeAt(index);
-  }
-  deleteSellingPoint2(index) {
-    this.sellingPoints2.removeAt(index);
+
+  deleteTurmasDisciplinas(index) {
+    this.turmasDisciplinas.removeAt(index);
   }
 
   private createForm() {
-    // this.form = this.formBuilder.group({
-    //   nome: ['', Validators.required],
-    //   cnpj: ['', Validators.required],
-    //   responsavel: ['', Validators.required],
-    // });
+
     this.form = this.formBuilder.group({
       nome: ['', Validators.required],
       perfil: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
-      turmas: this.formBuilder.group({
-
-      }),
-      disciplionas: [''],
+      turmasDisciplinas: this.formBuilder.array([this.formBuilder.group({
+        turma: ['', Validators.required],
+        disciplina: ['', Validators.required]
+      })])
     });
   }
 
   Adicionar() {
     this.isLoading = true;
     const result: usuario = Object.assign({}, this.form.value);
-    console.log(this.productForm.value);
-    console.log(this.productForm2.value);
     this.InstituicaoService
       .inserirInstituicao(result)
       .pipe(finalize(() => { this.isLoading = false; }))
@@ -106,7 +101,7 @@ list=[];
         this.isLoading = false;
 
         if (response) {
-          Swal.fire('Ok', 'Usuário adicionada com sucesso', 'success');
+          Swal.fire('Ok', 'Professor adicionada com sucesso', 'success');
           this.router.navigateByUrl("/instituicao/usuarios/professores");
 
         }
@@ -125,8 +120,6 @@ export class usuario {
   cpf: string = ''
   email: string = '';
   perfil: string = '';
-  turmas: string = '';
-  disciplinas: string = '';
-
+  turmasDisciplinas: any = [];
 }
 
